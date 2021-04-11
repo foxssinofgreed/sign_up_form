7fe8d1e8a2083c3ce042db5db8ee5d58b516afe5
fox <l_aptsiauri@cu.edu.ge>
2021-04-12 02:41:01 +0400

    configuring on MySQL

---
+++ php/create_database.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = 'localhost';
+$username = "newuser";
+$password = "password";
+$dbname = "sign_up_form";
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = 'CREATE TABLE users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    sign_up_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);';
+
+if ($connection->query($sql) === TRUE) {
+    echo "Table has been created successfully";
+} else {
+    echo "Error creating table: " . $connection->error;
+}
+
+$connection->close();
+
+?>
