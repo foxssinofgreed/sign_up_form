ab4c351a7580871f146d4778ba56382c3dcd21fc
fox <l_aptsiauri@cu.edu.ge>
2021-04-21 02:49:19 +0400

    Styling front

--- public/index.php
+++ public/index.php
@@ -3,12 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <link href="scss/style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!--<script src="js/"></script>-->
     <title>Title</title>
 </head>
 <body>
-    <div>
-
+    <div id="main_flex_box">
+        <div id="sing_up_form">
+            <div id="sign_up_inputs">
+                <div>
+                    <label>
+                        <img src="resources/icons/user.png" alt="user">
+                        <input type="text" placeholder="Username" name="user">
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <img src="resources/icons/email.png" alt="email">
+                        <input type="text" placeholder="Email" name="email">
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <img src="resources/icons/padlock.png" alt="lock">
+                        <input type="text" placeholder="Password" name="password">
+                    </label>
+                </div>
+            </div>
+            <div id="button"></div>
+        </div>
+        <div id="right_picture">
+            <img src="resources/images/fire2.gif" alt="boy_at_fire_camp">
+        </div>
     </div>
 </body>
 </html>
