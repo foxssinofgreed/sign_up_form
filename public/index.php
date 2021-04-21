338eb91fad2d605776967e8c714b0df7b7843ecf
fox <l_aptsiauri@cu.edu.ge>
2021-04-21 04:41:43 +0400

    input validation style

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link href="scss/style.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/ajax.js"></script>
     <!--<script src="js/"></script>-->
     <title>Title</title>
 </head>
@@ -20,13 +21,13 @@
                 <div>
                     <label>
                         <img src="resources/icons/email.png" alt="email">
-                        <input type="email" placeholder="Email" name="email" required>
+                        <input type="email" placeholder="Email" name="email" pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$" required>
                     </label>
                 </div>
                 <div>
                     <label>
                         <img src="resources/icons/padlock.png" alt="lock">
-                        <input type="password" placeholder="Password" name="password" required>
+                        <input id="password" type="password" placeholder="Password" name="password" minlength="8" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Minimum of 8 characters, should have at least one uppercase letter and two numbers." required>
                     </label>
                 </div>
             </div>
