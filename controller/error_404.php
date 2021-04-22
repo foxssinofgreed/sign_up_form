5cdff8a6b6a2a55db155a9672e279755c707442d
fox <l_aptsiauri@cu.edu.ge>
2021-04-23 03:26:09 +0400

    composer + 404 page styling

--- controller/error_404.php
+++ controller/error_404.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
 
+    <!-- local css -->
+    <link href="scss/style.css" rel="stylesheet">
+
+    <!-- window -->
     <title>404</title>
+    <link rel="icon" type="image/png" href="resources/404/minimalist_forest_transparent.gif">
 </head>
-<body>
-    <img src="resources/404/minimalist_forest_transparent.gif">
+<body class="error_404">
+<div id="main_flex_box">
+    <div id="sing_up_form">
+        <div class="text_404">
+            <h1>404</h1>
+            <h2>You've lost your way,<br>fire feels lonely without you.</h2>
+        </div>
+    </div>
+    <div id="right_picture">
+        <img src="resources/404/minimalist_forest_transparent.gif" alt="fire in the forest">
+        <div class="sun"></div>
+    </div>
+</div>
 </body>
 </html>
