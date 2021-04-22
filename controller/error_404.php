1f5c43a5d3d9d5cc517d3c9de4f67ad7367449e8
fox <l_aptsiauri@cu.edu.ge>
2021-04-23 01:48:48 +0400

    update

---
+++ controller/error_404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+
+    <title>404</title>
+</head>
+<body>
+    <img src="resources/404/minimalist_forest_transparent.gif">
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
+if ($_SERVER['REQUEST_URI'] == '/404' && $_SERVER["REQUEST_METHOD"] == "GET"){
+    require_once dirname(__FILE__).'/../controller/error_404.php';
+}
+
 if ($_SERVER['REQUEST_URI'] == '/'){
-    require_once dirname(__FILE__).'/../controller/sign_up.php';
+    if($_SERVER["REQUEST_METHOD"] == "GET"){
+        require_once dirname(__FILE__).'/../controller/sign_up.php';
+    }
+    else header('Location: /404');
 }
 
 if ($_SERVER['REQUEST_URI'] == '/registration'){
-    require_once dirname(__FILE__).'/../controller/registration.php';
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        require_once dirname(__FILE__).'/../controller/registration.php';
+    }
+    else header('Location: /404');
 }
