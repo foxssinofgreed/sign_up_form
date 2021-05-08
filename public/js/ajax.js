d8e6f4326d1de7f855a9f0643d6c8b19fd7db549
fox <l_aptsiauri@cu.edu.ge>
2021-05-08 14:40:05 +0400

    Ajax cleaner code

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
             'password': $('#password').val().trim()}
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:8000/registration',
+            url: '/registration',
             data: data,
             success: function (result){
                 $('#response_style').html(result);
