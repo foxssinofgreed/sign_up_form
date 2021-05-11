3a18baa80b72ea3fdf8c1230e5a05194bc512cfa
fox <l_aptsiauri@cu.edu.ge>
2021-05-12 01:05:08 +0400

    clearing inputs on successful register

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -17,7 +17,7 @@ $(document).ready(function (){
                     $('#error_message').css({'visibility': 'visible', 'background': 'url("resources/images/success.png")', 'opacity': '0.6'});
                     $('#error_message span')[0].innerHTML = 'You successfully registered';
                     setTimeout(function (){$('#error_message').attr('style', '');}, 5000);
-                    setTimeout(function (){$('#response_style').empty();}, 5000);
+                    setTimeout(function (){$('#response_style').empty(); $('label').find("input, textarea").val("");}, 5000);
                 }
                 if(result.includes('youre already here') && result.includes('its taken my boy')){
                     $('#error_message').css({'visibility': 'visible', 'opacity': '0.6'});
